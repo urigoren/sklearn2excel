d0781bfc4148424c330d490c3b3c0b4955020461
ugoren <uri@goren4u.com>
2020-04-25 17:09:35 +0300

    LogisticRegression Classifier

--- src/sklearn2excel.py
+++ src/sklearn2excel.py
@@ -1,14 +1,15 @@
+import numpy as np
 from string import ascii_uppercase
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 
 
-def argmax(xl_range):
-    return "MATCH(MAX({r}),{r},0)".format(r=xl_range)
+def argmax(x_range, y_range):
+    return "XLOOKUP(MAX({x}),{x},{y})".format(x=x_range, y=y_range)
 
 
-def argmin(xl_range):
-    return "MATCH(MIN({r}),{r},0)".format(r=xl_range)
+def argmin(x_range, y_range):
+    return "XLOOKUP(MIN({x}),{x},{y})".format(x=x_range, y=y_range)
 
 
 def column_letter(i: int) -> str:
@@ -18,30 +19,30 @@ def column_letter(i: int) -> str:
 
 
 def row_range(start_col: int, end_col: int, row_num: int) -> str:
-    return column_letter(start_col) + str(row_num) + ":" + column_letter(end_col) + str(row_num)
+    return "$" + column_letter(start_col) + str(row_num) + ":$" + column_letter(end_col) + str(row_num)
 
 
-def classes_array(model) -> str:
-    return "{" + ",".join(['"' + str(c).replace('"', '""') + '"' for c in model.classes_]) + "}"
+def array(lst) -> str:
+    return "{" + ",".join(['"' + str(c).replace('"', '""') + '"' for c in lst]) + "}"
+
+
+def np2array(X) -> str:
+    return "{" + ";".join([",".join([f"{a:0.3f}" for a in row]) for row in X]) + "}"
 
 
 def translate_log_reg(model: LogisticRegression) -> str:
     sigmoid = "1/(1+EXP(-{intercept:0.3f}-(SUM({{{weights}}}*{range}))))"
     if model.coef_.shape[0] == 1:  # Binary classification
-        xl_range = row_range(0, len(model.classes_), first_column)
+        xl_range = row_range(0, len(model.coef_)-1, first_column)
         weights = ",".join([f"{w:0.3f}" for w in model.coef_[0]])
         ret = sigmoid.format(intercept=float(model.intercept_), weights=weights, range=xl_range)
         ret = "ROUND({f},0)".format(f=ret)
-        ret = "INDEX({a}, {i}, 1)".format(a=classes_array(model), i=ret)
+        ret = "INDEX({a}, {i}, 1)".format(a=array(model.classes_), i=ret)
     else:  # multiclass
-        xl_range = row_range(0, len(model.classes_), first_column)
-        sigmoids = []
-        for i in range(model.coef_.shape[0]):
-            weights = ",".join([f"{w:0.3f}" for w in model.coef_[i]])
-            sigmoids.append(sigmoid.format(intercept=model.intercept_[i], weights=weights, range=xl_range))
-        sigmoids = "{" + ",".join(sigmoids) + "}"
-        ret = argmax(sigmoids)
-        ret = "INDEX({a}, {i}, 1)".format(a=classes_array(model), i=ret)
+        xl_range = row_range(0, model.coef_.shape[1]-1, first_column)
+        intercept = "{" + ",".join([f"{c:0.3f}" for c in model.intercept_]) + "}"
+        sigmoids = "1/(1-EXP(-{i}-MMULT({x},{w})))".format(w=np2array(model.coef_.T), x=xl_range, i=intercept)
+        ret = argmax(sigmoids, array(model.classes_))
     return "=" + ret.replace("(--", "(")
 
 
